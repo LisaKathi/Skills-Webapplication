82b8270dfecf176214a91398b9c35aae5194a692
Lisa Katharina Rumplmayr <33779925+LisaKathi@users.noreply.github.com>
2023-04-12 11:58:36 +0200

    fixing the chat function

--- chat-app-hsg/src/app/app.module.ts
+++ chat-app-hsg/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
@@ -14,7 +15,8 @@ import { ChatBoxComponent } from './components/chat-box/chat-box.component';
     ChatBoxComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
