9d6d87bdde7fbc681bc31e780c32bb7e9f108a65
knappsk <109874645+knappsk@users.noreply.github.com>
2023-05-17 03:06:06 +0200

    control functions for nickname input

--- chat-app-hsg/src/app/components/nickname/nickname.component.ts
+++ chat-app-hsg/src/app/components/nickname/nickname.component.ts
@@ -12,18 +12,45 @@ export class NicknameComponent {
 
   public nickname = '';
   public message = '';
+  public characterCount = 0;
+  public maxCharacterLimit = 15;
 
   public createNickname(nickname: string): void {
-    // Person.Nickname = nickname;
+
+    if (!nickname) {
+      this.message = 'Bitte geben Sie einen Nickname ein.';
+      return; // Exit the method if the nickname is empty
+    }
+
+    const regex = /^[a-zA-Z0-9]+$/; // Regular expression for alphanumeric characters
+
+  if (!regex.test(nickname)) {
+    this.message = 'Nur alphanumerische Zeichen sind erlaubt.';
+    this.nickname = '';
+    return; // Exit the method if invalid characters are found
+  }
+
       this.nicknameCreate.emit(nickname);
       setTimeout(() => {
         this.message = `Herzlich willkommen, ${nickname}!`;
     }, 2150);
   }
 
+  public onNicknameInput(event: any): void {
+    this.nickname = event.target.value;
+    this.characterCount = this.nickname.length;
+
+    if (this.characterCount >= this.maxCharacterLimit) {
+      event.target.value = event.target.value.slice(0, this.maxCharacterLimit);
+      this.characterCount = this.maxCharacterLimit;
+    }
+  }
+
   constructor(private el: ElementRef, private renderer: Renderer2, private router: Router) {}
 
-  animateButton() {
+  animateButton(nickname: string) {
+    const regex = /^[a-zA-Z0-9]+$/;
+    if (regex.test(nickname)) {
     const button = this.el.nativeElement.querySelector('.erstellen');
     //this.preventDefault()
     this.renderer.removeClass(button, 'animate');
@@ -34,8 +61,10 @@ export class NicknameComponent {
       this.renderer.removeClass(button, 'animate');
     }, 6000);
   }
-
-  redirectToStart() {
+  }
+  redirectToStart(nickname: string) {
+    const regex = /^[a-zA-Z0-9]+$/;
+    if (regex.test(nickname)) {
     const button = this.el.nativeElement.querySelector('.erstellen');
 
     setTimeout(() => {
@@ -44,5 +73,5 @@ export class NicknameComponent {
     }, 3000);
   }
   
-
+  }
   }
